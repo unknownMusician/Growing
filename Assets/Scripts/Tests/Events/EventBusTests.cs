71cf54cfcf7bee876b6088dfe6b3ba340e1e20c8
Serhii Ovchynnikov <unknown.musician.t@gmail.com>
2023-10-10 21:57:26 +0300

    Refactor tests folder

--- Assets/Scripts/Tests/Events/EventBusTests.cs
+++ Assets/Scripts/Tests/Events/EventBusTests.cs
@@ -1,42 +1,35 @@
-using System;
 using Growing.Events;
 using NUnit.Framework;
 
 namespace Growing.Tests.EditMode.Events
 {
-    public sealed class SubscribersOrdererMock : ISubscribersOrderer
-    {
-        private readonly Func<Type, int> order;
-
-        public SubscribersOrdererMock(Func<Type, int> order)
-        {
-            this.order = order;
-        }
-
-        public int Order(Type subscriberType) => order(subscriberType);
-    }
-
     public sealed class EventBusTests
     {
         [Test]
-        public void SubscribedHandler_Invoked_WhenSameTypePassed()
+        public void SubscribedHandler_SubscribedTypePassed_SubscriberInvoked()
         {
             // todo: should depend on interface
-            var eventBus = new EventBus(new SubscribersOrdererMock(_ => 0));
+            var eventBus = new EventBus(new SubscribersOrdererMock
+            {
+                Order = _ => 0,
+            });
             int handledValue = 0;
             const int firedValue = 5;
 
             eventBus.Subscribe<int>(this, x => handledValue = x);
             eventBus.Invoke<int>(firedValue);
 
-            Assert.That(handledValue == firedValue);
+            Assert.That(handledValue, Is.EqualTo(firedValue));
         }
 
         [Test]
-        public void SubscribedHandler_NotInvoked_WhenOtherTypePassed()
+        public void SubscribedHandler_SubscribedTypePassed_Ignored()
         {
             // todo: should depend on interface
-            var eventBus = new EventBus(new SubscribersOrdererMock(_ => 0));
+            var eventBus = new EventBus(new SubscribersOrdererMock
+            {
+                Order = _ => 0,
+            });
             const int initialValue = 0;
             const float firedValue = 5;
             int handledValue = initialValue;
@@ -44,10 +37,9 @@ namespace Growing.Tests.EditMode.Events
             eventBus.Subscribe<int>(this, x => handledValue = x);
             eventBus.Invoke<float>(firedValue);
 
-            Assert.That(handledValue == initialValue);
+            Assert.That(handledValue, Is.EqualTo(initialValue));
         }
-        
-        
+
 
         // todo: ordering
     }
---
+++ Assets/Scripts/Tests/Events/SubscribersOrdererMock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Growing.Events;
+
+namespace Growing.Tests.EditMode.Events
+{
+    public struct SubscribersOrdererMock : ISubscribersOrderer
+    {
+        public Func<Type, int> Order { get; set; }
+
+        int ISubscribersOrderer.Order(Type subscriberType) => Order(subscriberType);
+    }
+}
